cf4f8f5f3eecf3bda1f33d025eb857f216d71dca
= <=>
2026-03-12 23:43:22 -0700

    ready to deploy

--- server/src/server.js
+++ server/src/server.js
@@ -5,13 +5,15 @@ import http from 'http'
 import {connectdb} from  './config/config.js'
 import authRoute from './routes/auth.route.js'
 import messageRoute from './routes/message.route.js'
+import path from 'path'
+
 
 const app = express()     
 connectdb()
 
 app.use(express.json())
 
-
+const __dirname = path.resolve()
 // Routes
 
 app.use('/auth',authRoute)
@@ -26,8 +28,16 @@ io.on('connection',(socket)=>{
     socket.emit('hello')
 })
 
-app.get('/',(req,res)=>{res.send("hello")})
 
+
+if(process.env.NODE_ENV == "production")
+{
+    app.use(express.static(path.join(__dirname,"../client/dist")))
+
+ app.use((req, res) => {
+  res.sendFile(path.resolve(__dirname, "../../client/dist/index.html"));
+});
+}
 
 server.listen(process.env.PORT,()=>{console.log('server is running on http://localhost:3000');
 })

c94486e9928021e4a2091428c1e9ab937751888b
sabih <sabih.dev@gmail.com>
2026-03-15 13:38:53 +0500

    added the messages Routes with arjet ratelimmiter

--- server/src/controller/message.controller.js
+++ server/src/controller/message.controller.js
@@ -44,6 +44,20 @@ export const sendMessage = async (req, res) => {
         const id = req.params.id
         const LogedInUserId = req.user.id
         const { text, image } = req.body
+
+        if (!text && !image) {
+            return res.status(400).json({ message: "Text or image is required." });
+        }
+        if (senderId.equals(receiverId)) {
+            return res.status(400).json({ message: "Cannot send messages to yourself." });
+        }
+        const receiverExists = await User.exists({ _id: receiverId });
+        if (!receiverExists) {
+            return res.status(404).json({ message: "Receiver not found." });
+        }
+
+
+
         let message = text
         let imageURl
         if (image) {
